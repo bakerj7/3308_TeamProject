a4a8ec678befff8161bca93b7d87fafb9cfbc220
Thomas Nelson <thomas.nelson@colorado.edu>
2018-10-24 11:52:33 -0600

    Add favorite actor and input testing

--- movieApp.py
+++ movieApp.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 # enter the genre
 # search through list
+
+# Possible futre things
+# Weighting
+# cleaning inputs
+#
 print("Welcome to the world's greatest movie tool!")
-genre = input("What is your favorite genre? ")
+foundGenre = False
+genreList = ["Drama", "Action", "Comedy"]
+while not foundGenre:
+    print(genreList)
+    genre = input("What is your favorite genre? ")
+    if genre not in genreList:
+        print("bad input")
+    else:
+        foundGenre = True
+actor = input("Who is your favorite actor? ")
 for line in open('movieData.csv'):
-    if genre in line:
+    if genre in line or actor in line:
         print(line)
