4e306b1af2481a4927f24922d1d247f21affe3a8
gutibeni <jmgutierr@gmail.com>
2018-10-30 08:13:17 -0600

    Added username input and text file output

--- movieApp.py
+++ movieApp.py
@@ -7,8 +7,13 @@
 # cleaning inputs
 #
 print("Welcome to the world's greatest movie tool!")
+
+username = input("Please enter a username: ")
+while not username:
+    username = input("Please enter a valid username")
 foundGenre = False
-genreList = ["Drama", "Action", "Comedy"]
+genreList = ["Drama", "Action", "Comedy", "SciFi", "Documentary", "Romance", \
+             "Fantasy"]
 while not foundGenre:
     print(genreList)
     genre = input("What is your favorite genre? ")
@@ -20,3 +25,9 @@ actor = input("Who is your favorite actor? ")
 for line in open('movieData.csv'):
     if genre in line or actor in line:
         print(line)
+
+output_file = open(username + '.txt', 'w')
+output_file.write("Username: " + username + '\n')
+output_file.write("Genres: " + genre + '\n')
+output_file.write("Actors: " + actor + '\n')
+output_file.close()
